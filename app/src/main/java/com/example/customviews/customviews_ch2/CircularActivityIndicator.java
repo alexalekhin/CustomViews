74e29696fd85ca8497fabadbc31ff6d199637b7f
Aleksamdr <alekh.alvl@gmail.com>
2020-04-04 22:48:38 +0300

    ch3: introduce GestureDetector and Scroller
    - Use GestureDetector(and GestureListener) to handle gestures
    - Use Scroller to make value updates smoother

--- app/src/main/java/com/example/customviews/customviews_ch2/CircularActivityIndicator.java
+++ app/src/main/java/com/example/customviews/customviews_ch2/CircularActivityIndicator.java
@@ -6,8 +6,10 @@ import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Region;
 import android.util.AttributeSet;
+import android.view.GestureDetector;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Scroller;
 
 import androidx.annotation.ColorInt;
 
@@ -29,19 +31,21 @@ public class CircularActivityIndicator extends View {
     private Path clipPath;
 
     private boolean isPressed = false;
-    private float previousX, previousY;
+
+    private GestureDetector gestureDetector;
+    private Scroller angleScroller;
 
     public CircularActivityIndicator(Context context) {
         super(context);
-        init(DEFAULT_FG_COLOR, DEFAULT_BG_COLOR);
+        init(context, DEFAULT_FG_COLOR, DEFAULT_BG_COLOR);
     }
 
     public CircularActivityIndicator(Context context, AttributeSet attrs) {
         super(context, attrs);
-        init(DEFAULT_FG_COLOR, DEFAULT_BG_COLOR);
+        init(context, DEFAULT_FG_COLOR, DEFAULT_BG_COLOR);
     }
 
-    private void init(@ColorInt int foregroundColor, @ColorInt int backgroundColor) {
+    private void init(Context context, @ColorInt int foregroundColor, @ColorInt int backgroundColor) {
         foregroundPaint.setColor(foregroundColor);
         foregroundPaint.setStyle(Paint.Style.FILL);
 
@@ -49,10 +53,103 @@ public class CircularActivityIndicator extends View {
         backgroundPaint.setStyle(Paint.Style.FILL);
 
         selectedAngle = 280;
+
+        angleScroller = new Scroller(context, null, true);
+        angleScroller.setFinalX(selectedAngle);
+
+        gestureDetector = new GestureDetector(context, new GestureDetector.OnGestureListener() {
+            boolean isHandling;
+
+            @Override
+            public boolean onDown(MotionEvent e) {
+                isHandling = processAngleUpdate(e.getX(), e.getY());
+
+                if (isHandling) {
+                    getParent().requestDisallowInterceptTouchEvent(true);
+                    changePressed(true);
+                    postInvalidate();
+                }
+                return isHandling;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent e) {
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                endGesture();
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                isHandling = processAngleUpdate(e2.getX(), e2.getY());
+                postInvalidate();
+                return true;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+                endGesture();
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                endGesture();
+                return false;
+            }
+        });
+    }
+
+    private void endGesture() {
+        getParent().requestDisallowInterceptTouchEvent(false);
+        changePressed(false);
+        postInvalidate();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_UP) {
+            endGesture();
+            return true;
+        } else {
+            return gestureDetector.onTouchEvent(event);
+        }
+    }
+
+    private boolean processAngleUpdate(float x, float y) {
+        x -= (float) getWidth() / 2;
+        y -= (float) getHeight() / 2;
+
+        double radius = Math.sqrt(x * x + y * y);
+
+        if (radius > (float) circleSize / 2 || radius < circleSize * 0.75 / 2)
+            return false;
+
+        int angle = (int) (180.0 * Math.atan2(y, x) / Math.PI) + 90;
+        angle = (angle > 0) ? angle : 360 + angle;
+
+        if (angleScroller.computeScrollOffset()) {
+            angleScroller.forceFinished(true);
+        }
+
+        angleScroller.startScroll(angleScroller.getCurrX(), 0, angle - angleScroller.getCurrX(), 0);
+        return true;
+    }
+
+    private void changePressed(boolean isPressed) {
+        this.isPressed = isPressed;
+        invalidate();
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
+        boolean isScrollNotFinished = angleScroller.computeScrollOffset();
+        selectedAngle = angleScroller.getCurrX();
+
+        if (isScrollNotFinished) invalidate();
+
         foregroundPaint.setColor(isPressed ? PRESSED_FG_COLOR : DEFAULT_FG_COLOR);
 
         circleSize = getWidth();
@@ -113,60 +210,5 @@ public class CircularActivityIndicator extends View {
                 true,
                 foregroundPaint
         );
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-
-        boolean isHandled = false;
-
-        switch (event.getAction()) {
-            case MotionEvent.ACTION_DOWN:
-
-                previousX = event.getX();
-                previousY = event.getY();
-
-                isHandled = processAngleUpdate(previousX, previousY);
-
-                if (isHandled) {
-                    getParent().requestDisallowInterceptTouchEvent(true);
-                    changePressed(true);
-                }
-                return isHandled;
-
-            case MotionEvent.ACTION_UP:
-                changePressed(false);
-                return true;
-
-            case MotionEvent.ACTION_MOVE:
-                float currentX = event.getX();
-                float currentY = event.getY();
-
-                isHandled = processAngleUpdate(currentX, currentY);
-                invalidate();
-                return isHandled;
-
-            default:
-                return false;
-        }
-    }
-
-    private boolean processAngleUpdate(float x, float y) {
-        x -= (float) getWidth() / 2;
-        y -= (float) getHeight() / 2;
-
-        double radius = Math.sqrt(x * x + y * y);
-
-        if (radius > (float) circleSize / 2)
-            return false;
-
-        int angle = (int) (180.0 * Math.atan2(y, x) / Math.PI) + 90;
-        selectedAngle = (angle > 0) ? angle : 360 + angle;
-        return true;
-    }
-
-    private void changePressed(boolean isPressed) {
-        this.isPressed = isPressed;
-        invalidate();
     }
 }
