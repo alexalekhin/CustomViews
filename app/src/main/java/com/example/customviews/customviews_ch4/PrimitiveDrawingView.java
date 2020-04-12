b127dfddf7e19fbe3a06d5b4fe5136c5f974be59
Aleksamdr <alekh.alvl@gmail.com>
2020-04-12 08:03:00 +0300

    ch4: remake PrimitiveDrawingView for drawing bezier curves

--- app/src/main/java/com/example/customviews/customviews_ch4/PrimitiveDrawingView.java
+++ app/src/main/java/com/example/customviews/customviews_ch4/PrimitiveDrawingView.java
@@ -1,36 +1,33 @@
 package com.example.customviews.customviews_ch4;
 
 import android.content.Context;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Matrix;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.util.AttributeSet;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.ColorInt;
 
-import com.example.customviews.R;
+import java.util.ArrayList;
 
 public class PrimitiveDrawingView extends View {
 
     @ColorInt
-    private static final int DEFAULT_FG_COLOR = 0xffa0a0a0;
+    private static final int DEFAULT_FG_COLOR = 0xffff0000;
     @ColorInt
     private static final int DEFAULT_BG_COLOR = 0xff000221;
     @ColorInt
     private static final int BLACK_COLOR = 0xff000000;
     private static final int WHITE_COLOR = 0xffffffff;
 
-    public static final int POINTS_NUM = 20;
+    public static final int BEZIER_CURVE_POINTS_NUM = 3;
 
-    private Paint paint = new Paint();
-    private Path path;
-
-    private Bitmap backgroundBitmap;
-    private Matrix backgroundMatrix = new Matrix();
+    private Paint pathPaint = new Paint();
+    private Paint pointsPaint = new Paint();
+    private Path path = new Path();
+    private ArrayList<Float> pointList = new ArrayList<>();
 
     public PrimitiveDrawingView(Context context) {
         super(context);
@@ -43,48 +40,60 @@ public class PrimitiveDrawingView extends View {
     }
 
     private void init() {
-        paint.setStyle(Paint.Style.FILL);
-        paint.setAntiAlias(true);
-        paint.setColor(WHITE_COLOR);
+        pathPaint.setStyle(Paint.Style.STROKE);
+        pathPaint.setAntiAlias(true);
+        pathPaint.setColor(WHITE_COLOR);
+        pathPaint.setStrokeWidth(5.f);
 
-        backgroundBitmap = BitmapFactory.decodeResource(getResources(), R.drawable.harold);
+        pointsPaint.setStyle(Paint.Style.STROKE);
+        pointsPaint.setAntiAlias(true);
+        pointsPaint.setColor(DEFAULT_FG_COLOR);
+        pointsPaint.setStrokeCap(Paint.Cap.ROUND);
+        pointsPaint.setStrokeWidth(40.f);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            pointList.add(event.getX());
+            pointList.add(event.getY());
+
+            invalidate();
+        }
+        return super.onTouchEvent(event);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawColor(DEFAULT_BG_COLOR);
 
-        if (path == null) {
-            float[] pointsArray = new float[POINTS_NUM * 3];
-            for (int i = 0; i < POINTS_NUM; i++) {
-                pointsArray[i * 3] = (float) Math.random() * getWidth();
-                pointsArray[i * 3 + 1] = (float) Math.random() * getHeight();
-                pointsArray[i * 3 + 2] = (float) Math.random() * ((float) getWidth() / 4);
-            }
+        int currentIndex = 0;
 
-            path = new Path();
+        while (pointList.size() - currentIndex >= BEZIER_CURVE_POINTS_NUM * 2) {
+            float x1 = pointList.get(currentIndex);
+            float y1 = pointList.get(currentIndex + 1);
 
-            for (int i = 0; i < pointsArray.length / 3; i++) {
-                path.addCircle(
-                        pointsArray[i * 3],
-                        pointsArray[i * 3 + 1],
-                        pointsArray[i * 3 + 2],
-                        Path.Direction.CW
-                );
+            float x2 = pointList.get(currentIndex + 2);
+            float y2 = pointList.get(currentIndex + 3);
+
+            float x3 = pointList.get(currentIndex + 4);
+            float y3 = pointList.get(currentIndex + 5);
+
+            if (currentIndex == 0) {
+                path.moveTo(x1, y1);
             }
-            path.close();
+            path.cubicTo(x1, y1, x2, y2, x3, y3);
+
+            currentIndex += 6;
         }
 
-        canvas.save();
+        canvas.drawPath(path, pathPaint);
 
-        canvas.clipPath(path);
-        if (backgroundBitmap != null) {
-            backgroundMatrix.reset();
-            float scale = (float) getWidth() / backgroundBitmap.getWidth();
-            backgroundMatrix.postScale(scale, scale);
-            canvas.drawBitmap(backgroundBitmap, backgroundMatrix, null);
+        for (int i = 0; i < pointList.size() / 2; i++) {
+            float x = pointList.get(i * 2);
+            float y = pointList.get(i * 2 + 1);
+
+            canvas.drawPoint(x, y, pointsPaint);
         }
-
-        canvas.restore();
     }
 }
